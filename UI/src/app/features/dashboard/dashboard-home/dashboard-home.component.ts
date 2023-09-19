dd8e51de2cffdb4cff7409dddcfe586fbf94d5e6
Joseph Lund <skleb.programming@gmail.com>
2023-09-18 17:15:32 -0700

    fixing ui more

--- UI/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
+++ UI/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthenticationService } from 'src/app/core/services/auth.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -9,6 +9,9 @@ import { ProductsSoldParent } from 'src/app/core/models/productsSoldParent';
 import { GenericType } from 'src/app/core/models/genericType';
 import { Payment } from 'src/app/core/models/payment';
 import { NotificationService } from 'src/app/core/services/notification.service';
+import { User } from 'src/app/core/models/user';
+import { UserModal } from '../../users/userModal/user-modal.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -22,19 +25,26 @@ export class DashboardHomeComponent implements OnInit {
   rangeForm!: FormGroup;
   creatorForm!: FormGroup;
   creators: GenericType[] = [];
+
+  userList: User[] = [];
   owed: Payment | undefined;
+  roleList: GenericType[] = [];
+  collectionList: GenericType[] = [];
+  isColumnsMobile: boolean = false;
+  isLoadingResults: boolean = false;
   payments: Payment[] = [];
   productsSold: ProductsSoldParent = new ProductsSoldParent(0, 0, []);
   isAdmin = false;
   selectedTab: string = 'Analytics'
   breakpoint = 0;
-  displayedColumns: string[] = ['ProductName', 'ProfitShare', 'Count'];
+  displayedColumns: string[] = [];
 
   constructor(private userService: UserService,
     private authService: AuthenticationService,
     private dashboardService: DashboardService,
     private notificationService: NotificationService,
-    private titleService: Title) {
+    private titleService: Title,
+    private dialog: MatDialog) {
     this.rangeForm = new FormGroup({
       start: new FormControl(new Date(), Validators.required),
       end: new FormControl(new Date(), Validators.required)
@@ -48,6 +58,11 @@ export class DashboardHomeComponent implements OnInit {
     this.currentUser = this.authService.getCurrentUser();
     this.isAdmin = this.currentUser.isAdmin;
     this.titleService.setTitle('Brickalytics - Dashboard');
+
+    this.onResizeInit();
+    this.getUsersList();
+    this.getRolesList();
+    this.getCollectionsList();
     this.toggleTab('Analytics');
   }
 
@@ -120,6 +135,10 @@ export class DashboardHomeComponent implements OnInit {
     }, ()=>{},()=>{
       this.onResizeInit();
     });
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 
   getProductsSoldAdmin() {
@@ -195,6 +214,118 @@ export class DashboardHomeComponent implements OnInit {
     var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
     return lastWeek;
   }
+
+
+  getUsersList() {
+    this.userService.getUsers().subscribe(res => {
+      if(res.code == 200){
+      this.userList = res.data!;
+      } else {
+      this.notificationService.openSnackBar(res.message);
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.userService.addUser(user).subscribe(res => {
+      if (res.code == 200) {
+        user.id = res.data;
+        this.userList!.push(user);
+      } else {
+        this.notificationService.openSnackBar(res.message);
+      }
+    });
+  }
+  updateUser(user: User) {
+    this.userService.updateUser(user).subscribe(res => {
+      if (res.code == 200) {
+        this.getUsersList();
+      } else {
+        this.notificationService.openSnackBar(res.message)
+      }
+    });
+  }
+  updateUserPassword(user: User, password: string) {
+    this.userService.updateUserPassword(user.id!, password).subscribe(res => {
+      if (res.code != 200) {
+        this.notificationService.openSnackBar(res.message);
+      }
+    });
+  }
+  getRole(id: number | null) {
+    for (var role of this.roleList!) {
+      if (role.id == id) {
+        return role.name;
+      }
+    }
+    return "None";
+  }
+  getCollection(id: number) {
+    if (id) {
+      for (var collection of this.collectionList!) {
+        if (collection.id == id) {
+          return collection.name;
+        }
+      }
+    }
+    return "None";
+  }
+  openUserModal(user: User | null = null) {
+
+    if (!user) {
+      user = new User(null, true, 2, 0, '', null);
+    }
+
+    const dialogRef = this.dialog.open(UserModal, {
+      data: { user: user, roleList: this.roleList, collectionList: this.collectionList }
+    });
+
+    dialogRef.afterClosed().subscribe(_user => {
+      this.getUsersList();
+    });
+  }
+  getRolesList() {
+    this.userService.getRoles().subscribe(res => {
+      if(res.code == 200){
+      this.roleList = res.data!;
+      } else {
+
+      console.error(res.code, res.message);
+      this.notificationService.openSnackBar(res.message);
+      }
+    });
+  }
+  getCollectionsList() {
+    this.userService.getCollections().subscribe(res => {
+      if(res.code == 200){
+      this.collectionList = res.data!;
+      } else {
+      this.notificationService.openSnackBar(res.message);
+      }
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+
+    if (event.target.innerWidth < 992 && !this.isColumnsMobile) {
+
+      this.displayedColumns = ['CreatorName', 'Actions'];
+      this.isColumnsMobile = true;
+
+    } else if (event.target.innerWidth >= 992 && this.isColumnsMobile) {
+
+      this.displayedColumns = ['CreatorName', 'Active', 'Role', 'Collection', 'Actions'];
+      this.isColumnsMobile = false;
+    }
+    if (event.target.innerWidth >= 1400) { this.breakpoint = 5 };
+    if (event.target.innerWidth < 1400) { this.breakpoint = 4 };
+    if (event.target.innerWidth < 1200) { this.breakpoint = 3 };
+    if (event.target.innerWidth < 992) { this.breakpoint = 2 };
+    if (event.target.innerWidth < 550) { this.breakpoint = 1 };
+
+  }
+
   onResizeInit() {
     if (window.innerWidth >= 1400) { this.breakpoint = 5 };
     if (window.innerWidth < 1400) { this.breakpoint = 4 }
@@ -202,11 +333,5 @@ export class DashboardHomeComponent implements OnInit {
     if (window.innerWidth < 992) { this.breakpoint = 2 }
     if (window.innerWidth < 550) { this.breakpoint = 1 }
   }
-  onResize(event: any) {
-    if (event.target.innerWidth >= 1400) { this.breakpoint = 5 };
-    if (event.target.innerWidth < 1400) { this.breakpoint = 4 };
-    if (event.target.innerWidth < 1200) { this.breakpoint = 3 };
-    if (event.target.innerWidth < 992) { this.breakpoint = 2 };
-    if (event.target.innerWidth < 550) { this.breakpoint = 1 };
-  }
+
 }
